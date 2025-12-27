3ee80d707ebf357688602454ad4bc8ed3ea10bbb
MajesTech-FTC <ftc32068@gmail.com>
2025-12-26 18:10:42 -0700

    Update TeleOp2.java
    12/26/25

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp2.java
@@ -1,17 +1,12 @@
 package org.firstinspires.ftc.teamcode;
 
-import static android.os.SystemClock.sleep;
 import static com.qualcomm.robotcore.hardware.DcMotorSimple.Direction.FORWARD;
-import static com.qualcomm.robotcore.hardware.DcMotorSimple.Direction.REVERSE;
 
 import com.pedropathing.follower.Follower;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
 
 import com.qualcomm.hardware.limelightvision.LLResult;
-import com.qualcomm.hardware.limelightvision.LLResultTypes;
-import com.qualcomm.hardware.limelightvision.LLStatus;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 
 import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
@@ -21,6 +16,13 @@ import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 public class TeleOp2 extends OpMode {
 private Follower follower;
 private Limelight3A limelight;
+public DcMotor shooter;
+private double power = 0.25;
+
+DcMotor FL;
+DcMotor FR;
+DcMotor BL;
+DcMotor BR;
 
 
 
@@ -29,12 +31,20 @@ private Limelight3A limelight;
         follower = Constants.createFollower(hardwareMap);
         follower.update();
 
+        shooter = hardwareMap.get(DcMotor.class, "shooter");
+
+        shooter.setDirection(FORWARD);
 
         limelight = hardwareMap.get(Limelight3A.class, "limelight");
 
         telemetry.setMsTransmissionInterval(11);
 
         limelight.pipelineSwitch(0);
+
+        FL = hardwareMap.get(DcMotor.class, "FrontLeftDrive");
+        FR = hardwareMap.get(DcMotor.class, "FrontRightDrive");
+        BL = hardwareMap.get(DcMotor.class, "BackLeftDrive");
+        BR = hardwareMap.get(DcMotor.class, "BackRightDrive");
     }
 
     @Override
@@ -50,10 +60,40 @@ private Limelight3A limelight;
         follower.update();
 
         //joe builder
-        follower.setTeleOpDrive(gamepad1.left_stick_y, gamepad1.left_stick_x, -gamepad1.right_stick_x);
-
+        follower.setTeleOpDrive(-gamepad1.left_stick_y, gamepad1.left_stick_x, -gamepad1.right_stick_x);
 
         LLResult result = limelight.getLatestResult();
+
+        if (gamepad1.dpad_up) {
+            shooter.setPower(0.60);
+        }
+        if (gamepad1.xWasPressed()) {
+            shooter.setPower(0);
+        }
+
+        if (gamepad1.b) {
+            while (result.getTx() != 0) {
+                if (result.getTx() < 0) {
+                    spinLeft();
+                } else if (result.getTx() > 0) {
+                    spinRight();
+                } else {
+                    brake();
+                }
+            }
+        }
+
+        if (gamepad1.a) {
+            if (result.getTx() < 0) {
+                spinLeft();
+            } else if (result.getTx() > 0) {
+                spinRight();
+            } else {
+                brake();
+            }
+        }
+
+
         if (result != null) {
             if (result.isValid()) {
                 Pose3D botpose = result.getBotpose();
@@ -62,6 +102,24 @@ private Limelight3A limelight;
                 telemetry.addData("Botpose", botpose.toString());
             }
         }
+    }
+
+    private void spinRight() {
+        BL.setDirection(DcMotor.Direction.REVERSE);
+        FL.setDirection(DcMotor.Direction.REVERSE);
+        FL.setPower(power); BL.setPower(power); FR.setPower(-power); BR.setPower(-power);
+    }
+
+    private void spinLeft() {
+        BL.setDirection(DcMotor.Direction.REVERSE);
+        FL.setDirection(DcMotor.Direction.REVERSE);
+        FL.setPower(-power); BL.setPower(-power); FR.setPower(power); BR.setPower(power);
+    }
+
+    private void brake() {
+        BL.setDirection(DcMotor.Direction.REVERSE);
+        FL.setDirection(DcMotor.Direction.REVERSE);
+        FL.setPower(0); BL.setPower(0); FR.setPower(0); BR.setPower(0);
     }
 
 }
