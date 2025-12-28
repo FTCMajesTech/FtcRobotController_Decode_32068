1cb7ce139999e1ae4a779c525910e26a2aaef407
MajesTech-FTC <ftc32068@gmail.com>
2025-12-28 12:45:40 -0700

    Update TeleOp3.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp3.java
@@ -17,30 +17,38 @@ private Follower follower;
 private Limelight3A limelight;
 public DcMotor shooter;
 private double power = 0.25;
-
+//how fast robot rotates when auto-aligning
 
 
 
     @Override
     public void init() {
+        //creates drive follower
         follower = Constants.createFollower(hardwareMap);
         follower.update();
 
+        //connects shooter and sets direction
         shooter = hardwareMap.get(DcMotor.class, "shooter");
-
         shooter.setDirection(FORWARD);
 
+        //connects limelight
         limelight = hardwareMap.get(Limelight3A.class, "limelight");
 
+        //telemetry updates every 11ms
         telemetry.setMsTransmissionInterval(11);
 
+        //sets limelight pipeline(set of vision settings saved inside the Limelight) to 0
         limelight.pipelineSwitch(0);
+        //pipeline 0= AprilTag detection
+        //pipeline 1= color detection
+        //pipeline 2= backup/testing
+        //pipeline tells camera: what to look for(AprilTags, color blobs, etc.), how to process the image, and what data to output(Tx, Ty, pose, tags)
 
     }
 
     @Override
     public void start() {
-
+        //enables TeleOp driving and starts limelight
         follower.startTeleOpDrive();
         limelight.start();
 
@@ -50,10 +58,10 @@ private double power = 0.25;
     public void loop() {
         follower.update();
 
-        //joe builder
+        //updates drive system(left stick Y: forward/back, left stick X: strafe, right stick X: turn)
         follower.setTeleOpDrive(-gamepad1.left_stick_y, gamepad1.left_stick_x, -gamepad1.right_stick_x);
 
-        //VANILLER CHANGE THE COMMENTS TO BE GOOD
+        //gets latest vision result
         LLResult result = limelight.getLatestResult();
 
         if (gamepad1.dpad_up) {
@@ -63,6 +71,10 @@ private double power = 0.25;
             shooter.setPower(0);
         }
 
+
+        //tries to rotate until Tx is 0 and uses a while loop
+        //can also freeze robot (CHANGE THIS PART OF CODE)
+        //while loops can lock robot
         if (gamepad1.b) {
             while (result.getTx() != 0) {
                 if (result.getTx() < 0) {
@@ -75,6 +87,9 @@ private double power = 0.25;
             }
         }
 
+        //Tx:how far the target is left/right
+        //if target is left: spin left
+        //if target is more right: spin right
         if (gamepad1.a) {
             if (result.getTx() < 0) {
                 spinLeft();
@@ -89,14 +104,19 @@ private double power = 0.25;
         if (result != null) {
             if (result.isValid()) {
                 Pose3D botpose = result.getBotpose();
+                //horizontal error
                 telemetry.addData("tx", result.getTx());
+                //vertical error
                 telemetry.addData("ty", result.getTy());
+                //robot position
                 telemetry.addData("Botpose", botpose.toString());
+                //detected AprilTags
                 telemetry.addData("april tag", result.getFiducialResults());
             }
         }
     }
 
+    //helper movement methods
     private void spinRight() {
         // Arguments: (forward, strafe, rotation)
         // 0 forward, 0 strafe, and positive power for rotation
@@ -108,6 +128,7 @@ private double power = 0.25;
         follower.setTeleOpDrive(0, 0, power);
     }
 
+    //stop all movement
     private void brake() {
         // Sets all movement vectors to zero
         follower.setTeleOpDrive(0, 0, 0);
